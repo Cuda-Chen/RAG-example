8e8585e0caa01501eb98e216a1079a58cfe05d75
Cuda-Chen <clh960524@gmail.com>
2019-04-25 13:27:31 +0800

    add argsparse feature

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ A scikit-image RAG example.
 
 # How to Install and Run
 ```
-# create a virtural environment
+# create a virtural environment (recommended)
 $ pip install -r requirements.txt
-$ python main.py
+$ python main.py [input image path] [number of superpixel] [compactness] [threshold for RAG]
 ```
+
+# Where is my output image?
+It should locate at the directory as same as your input image
+
+# Reference
+This work is originated from [A SIMPLE PROGRAMMER'S BLOG](https://vcansimplify.wordpress.com/2014/07/06/scikit-image-rag-introduction/).
+You can visit his work in the following link:
+* https://vcansimplify.wordpress.com/2014/07/06/scikit-image-rag-introduction/
--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 from skimage import data, io, segmentation, color
-from matplotlib import pyplot as plt
 from skimage.measure import regionprops
 from skimage.future import graph
 import numpy as np
+import argparse
+import os
 
 def show_image(image):
-    width = 10.0
-    height = image.shape[0] * width / image.shape[1]
-    f = plt.figure(figsize=(width, height))
-    plt.imshow(image)
-    plt.show()
+    io.imshow(image)
+    io.show()
 
-img = data.coffee()
-show_image(img)
+parser = argparse.ArgumentParser()
+parser.add_argument('input_image', type=str, help='input image path')
+parser.add_argument('num_superpixel', type=int, help='number of segments')
+parser.add_argument('compactness', type=int, help='compactness param of SLIC')
+parser.add_argument('thresh', type=float, help='threshold of combining edge')
+args = parser.parse_args()
 
-labels = segmentation.slic(img, compactness=30, n_segments=400)
+img = io.imread(args.input_image)
+outputfile = os.path.splitext(args.input_image)[0] \
++ '_' + str(args.num_superpixel) \
++ '_' + str(args.compactness) \
++ '_' + str(args.thresh) + '.bmp'
+#show_image(img)
+
+labels = segmentation.slic(img, n_segments=args.num_superpixel, compactness=args.compactness)
 labels = labels + 1 # so that regionprops will not to ignore no labelled region
 regions = regionprops(labels)
 
 label_rgb = color.label2rgb(labels, img, kind='avg')
-show_image(label_rgb)
+#show_image(label_rgb)
 
-label_rgb = segmentation.mark_boundaries(label_rgb, labels, (0, 0, 0))
-show_image(label_rgb)
+# uncomment these two lines if you want the SLIC output with edge boundary
+#label_rgb = segmentation.mark_boundaries(label_rgb, labels, (0, 0, 0))
+#show_image(label_rgb)
 
 rag = graph.rag_mean_color(img, labels)
 
-labels1 = graph.cut_threshold(labels, rag, 29)
+labels1 = graph.cut_threshold(labels, rag, args.thresh)
 out = color.label2rgb(labels1, img, kind='avg')
-show_image(out)
+#show_image(out)
+
+io.imsave(outputfile, out)
